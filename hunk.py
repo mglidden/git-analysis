4d52ee36303cfb47eedf5a545ed6fa6cd2c58221
Mason Glidden <mglidden@yelp.com>
2013-12-03 13:42:35 -0500

    Adding index on hunk.

--- hunk.py
+++ hunk.py
@@ -9,7 +9,7 @@ class Hunk(common.Base):
   id = Column(Integer, primary_key=True)
   lines_added = Column(Integer)
   lines_removed = Column(Integer)
-  file_diff_id = Column(Integer, ForeignKey('file_diffs.id'))
+  file_diff_id = Column(Integer, ForeignKey('file_diffs.id'), index=True)
   file_diff = relationship('FileDiff', backref='hunks')
 
   def __init__(self, lines_added=-1, lines_removed=-1):
