588c082fa68afcfa545f25e922cfbce1f4b45df1
Mason Glidden <mglidden@yelp.com>
2013-12-08 17:53:09 -0500

    Improved classifier to 70%. Added more classifiers and do a grid search on c and gamma.

--- classify.py
+++ classify.py
@@ -6,14 +6,35 @@ from hunk import Hunk
 
 from collections import Counter, defaultdict
 import csv
+import numpy as np
 from sklearn import svm
+from sklearn.cross_validation import StratifiedKFold
+from sklearn.grid_search import GridSearchCV
 import string
 
 session = common.Session()
 
-training_file = open(config.TRAINING_DATA_PATH, 'r')
-training_samples = list(csv.reader(training_file))
-training_file.close()
+
+should_remove_tweaks = False
+def remove_tweaks(samples):
+  return map(lambda (feature, commit_id): (min(6, int(feature)), commit_id), samples)
+
+should_remove_unknowns = True
+def remove_unknowns(samples):
+  return filter(lambda (feature, commit_id): feature != 1, samples)
+
+def load_samples_from_file(filename):
+  samples_file = open(filename, 'r')
+  samples = list(csv.reader(samples_file))
+  samples_file.close()
+  samples = map(lambda (feature, commit_id): (int(feature), int(commit_id)), samples)
+  if should_remove_tweaks:
+    samples = remove_tweaks(samples)
+  if should_remove_unknowns:
+    samples = remove_unknowns(samples)
+  return samples
+
+training_samples = load_samples_from_file(config.TRAINING_DATA_PATH)
 
 def is_merge(commit):
   return int(commit.is_merge)
@@ -34,8 +55,16 @@ def lines_removed(commit):
     return 0
   return min(1.0, commit.patch.lines_added() / 1000.0)
 
+def lines_ratio(commit):
+  if not commit.patch or commit.patch.lines_added() + commit.patch.lines_removed() == 0:
+    return 0.5
+  return commit.patch.lines_added() / float(commit.patch.lines_added() + commit.patch.lines_removed())
+
+def clean_or_refactor_in_message(commit):
+  return 'clean' in commit.message.lower() or 'refactor' in commit.message.lower()
+
 def get_words_from_message(commit_message):
-  cleaned_message = str(commit_message.replace('\n', ' ')).translate(string.maketrans('', ''), string.punctuation + '\t')
+  cleaned_message = str(commit_message.replace('\n', ' ')).translate(string.maketrans('', ''), string.punctuation + '\t').lower()
   return set(cleaned_message.split(' '))
 
 word_frequencies = Counter()
@@ -43,7 +72,7 @@ for _, commit_id in training_samples:
   commit = session.query(Commit).filter(Commit.id == commit_id).first()
   for word in get_words_from_message(commit.message):
     word_frequencies[word] += 1
-all_words = [word for word, _ in word_frequencies.most_common(20)]
+all_words = [word for word, _ in word_frequencies.most_common(800)]
 
 def word_features(commit):
   words = get_words_from_message(commit.message)
@@ -52,7 +81,7 @@ def word_features(commit):
 training_features = []
 training_classifications = []
 
-feature_creators = [is_merge, files_modified, lines_added, lines_removed]
+feature_creators = [is_merge, files_modified, lines_added, lines_removed, lines_ratio, clean_or_refactor_in_message]
 def create_features(commit):
   return [feature_creator(commit) for feature_creator in feature_creators] + word_features(commit)
 
@@ -64,16 +93,27 @@ for classification, commit_id in training_samples:
 
 
 print 'Training SVC'
-clf = svm.SVC()
-clf.fit(training_features, training_classifications)
+do_grid_search = True
+if do_grid_search:
+  C_range = 2.0 ** np.arange(-2, 20)
+  C_range = np.arange(0, 4000, 250)
+  gamma_range = 10 ** np.arange(-5, 4)
+  param_grid = dict(gamma=gamma_range, C=C_range)
+  cv = StratifiedKFold(y=training_classifications, n_folds = 3)
+  grid = GridSearchCV(svm.SVC(class_weight='auto'), param_grid=param_grid, cv=cv)
+  grid.fit(training_features, training_classifications)
+  clf = grid.best_estimator_
+  print clf
+else:
+  clf = svm.SVC(C=1500)
+  clf.fit(training_features, training_classifications)
 
 
 print 'Creating testing features'
-testing_file = open(config.TESTING_DATA_PATH, 'r')
-testing_reader = csv.reader(testing_file)
+testing_samples = load_samples_from_file(config.TESTING_DATA_PATH)
 testing_features = []
 testing_truth_classifications = []
-for classification, commit_id in testing_reader:
+for classification, commit_id in testing_samples:
   testing_features.append(create_features(session.query(Commit).filter(Commit.id == commit_id).first()))
   testing_truth_classifications.append(classification)
 
@@ -81,21 +121,29 @@ def classify_and_grade(features, truth, classifier):
   correct = 0
   incorrect = 0
   predictions = classifier.predict(features)
-  score_per_score = defaultdict(lambda: (0, 0));
+  score_per_class = defaultdict(lambda: {True: 0, False: 0, 'distribution': defaultdict(int)});
 
   for truth, guess in zip(truth, predictions):
     if truth == guess:
       correct += 1
-      defaultdict[truth][0] += 1
     else:
       incorrect += 1
-      defaultdict[truth][1] += 1
+    score_per_class[truth][truth == guess] += 1
+    score_per_class[truth]['distribution'][guess] += 1
   return (correct, incorrect, score_per_class)
+
+def pretty_print_results(results):
+  correct, incorrect, score_per_class = results
+  print 'Correct: %s\nIncorrect: %s\nPrecent: %s' % (correct, incorrect, correct / float(correct + incorrect))
+  for classname in sorted(score_per_class.keys()):
+    class_results = score_per_class[classname]
+    print '%s: %s/%s %s' % (classname, class_results[True], class_results[True] + class_results[False], class_results['distribution'])
+  print ''
 
 
 print 'Classifying the testing features.'
-print classify_and_grade(testing_features, testing_truth_classifications, clf)
+pretty_print_results(classify_and_grade(testing_features, testing_truth_classifications, clf))
 
 
 print 'Classifying the training features.'
-print classify_and_grade(training_features, training_classifications, clf)
+pretty_print_results(classify_and_grade(training_features, training_classifications, clf))
--- patch.py
+++ patch.py
@@ -9,16 +9,22 @@ class Patch(common.Base):
   id = Column(Integer, primary_key=True)
   diff = Column(String)
   files_changed = Column(Integer)
+  _lines_added = None
+  _lines_removed = None
 
   def __init__(self, diff='', files_changed=-1):
     self.diff = diff
     self.files_changed = files_changed
 
   def lines_added(self):
-    return sum([diff.lines_added() for diff in self.file_diffs])
+    if self._lines_added == None:
+      self._lines_added = sum([diff.lines_added() for diff in self.file_diffs])
+    return self._lines_added
 
   def lines_removed(self):
-    return sum([diff.lines_removed() for diff in self.file_diffs])
+    if self._lines_removed == None:
+      self._lines_removed = sum([diff.lines_removed() for diff in self.file_diffs])
+    return self._lines_removed
 
   def __repr__(self):
     return '<Patch(%s)>' % (self.id)
