f4e9111a3356a3ced4ca0592eea1e8a848f74656
Mason Glidden <mglidden@yelp.com>
2013-12-09 15:37:16 -0500

    Authors list on main page.

--- visualization/main.py
+++ visualization/main.py
@@ -1,8 +1,11 @@
 import fix_paths
 
+from models.author import Author
+from models.commit import Commit
 import common
 
 from flask import Flask, render_template
+from sqlalchemy import func
 
 app = Flask('GitAnalysis')
 app.debug = True
@@ -10,7 +13,8 @@ session = common.Session()
 
 @app.route('/')
 def main_page():
-  return render_template('layout.html')
+  authors = session.query(Author).join(Author.commits).group_by(Author.email).order_by(func.count(Author.commits).desc())
+  return render_template('repo.html', authors=authors)
 
 if __name__ == '__main__':
   app.run()
