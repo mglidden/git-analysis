6a488349928ae5025cd56cf833a7729818e0d36c
Mason Glidden <mglidden@yelp.com>
2013-12-10 02:58:55 -0500

    Adding back button to authors page.

--- visualization/static/js/author.js
+++ visualization/static/js/author.js
@@ -1,9 +1,21 @@
-var openAuthor = function(authorId) {
-  console.log(authorId);
-  d3.json('/author_classification.json/' + authorId, function(data) {
+var _loadChartData = function(dataURL) {
+  d3.json(dataURL, function(data) {
     d3.select('#stacked-chart svg')
       .datum(data)
       .transition().duration(500)
       .call(stackedChart);
   });
+}
+
+var openAuthor = function(authorId) {
+  console.log(authorId);
+  _loadChartData('/author_classification.json/' + authorId);
+
+  $('#authors-back').slideDown();
+}
+
+var openRepo = function() {
+  _loadChartData('/repo_classification.json');
+
+  $('#authors-back').slideUp();
 }
