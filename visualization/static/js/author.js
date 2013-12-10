f5d8aac3c4da1aa0946bebd143b752e4cb89cfec
Mason Glidden <mglidden@yelp.com>
2013-12-10 02:20:01 -0500

    Can now click on authors to get their commit frequency.

--- visualization/static/js/author.js
+++ visualization/static/js/author.js
@@ -1,3 +1,9 @@
 var openAuthor = function(authorId) {
   console.log(authorId);
+  d3.json('/author_classification.json/' + authorId, function(data) {
+    d3.select('#stacked-chart svg')
+      .datum(data)
+      .transition().duration(500)
+      .call(stackedChart);
+  });
 }
